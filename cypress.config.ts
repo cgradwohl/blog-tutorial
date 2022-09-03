33b2dd23547001b015460c21b997eea06dc0b583
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2022-09-03 14:58:31 -0700

    make smokey pass.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'cduyrt',
   e2e: {
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
