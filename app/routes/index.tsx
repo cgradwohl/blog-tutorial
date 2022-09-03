445474cb773522f2ba41ad8efb72b01f146f945b
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2022-09-03 13:12:49 -0700

    init, first routes and loader.

---
+++ app/routes/index.tsx
@@ -0,0 +1,18 @@
+import Vimeo from "@u-wave/react-vimeo";
+import { Link } from "@remix-run/react";
+
+export default function Index() {
+  return (
+    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <div className="relative sm:pb-16 sm:pt-8">
+        <div className="mx-auto mt-16 max-w-7xl text-center">
+          <Link to="/posts" className="text-xl text-blue-600 underline">
+            Blog Posts
+          </Link>
+
+          <Vimeo video="746047422" width={900} />
+        </div>
+      </div>
+    </main>
+  );
+}
